697614fd36e909b094c37937093211eeeb025430
Nivre0 <timleu256@gmail.com>
2026-01-09 12:59:58 +0100

    moved analytics component

--- pd-page/src/App.jsx
+++ pd-page/src/App.jsx
@@ -39,8 +39,8 @@ export default function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
-      </BrowserRouter>
       <Analytics />
+      </BrowserRouter>
     </div>
   );
 }
